91dbb6c15ebf8e28b13bb65c418a4349deb8ceb0
abhi <mulayabhishek92@gmail.com>
2025-06-10 17:18:46 +0530

    add

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-console.log("hello");
+let express = require("express");
+let bodyparser = require("body-parser");
+
+
+let app=express();
+
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.json());
+
+app.use(express.static("public"));
